483e5bba6d7d24f6290f5191bbbe355f8959eac2
root <jason.daly@goautomate.ai>
2024-01-01 12:43:46 -0500

    Fixed bug where Avaturn action map was missing

--- actionMap.js
+++ actionMap.js
@@ -26,6 +26,30 @@ const ACTION_MAP =
             }
         }
 
+    },
+
+    avaturn:
+    {
+        female:
+        {   idle: 
+            {   url: `${baseUrl}/animation-library-glb/feminine/idle/F_Standing_Idle_001.glb`,
+                source: "readyPlayerMe",
+                isDefault: true
+            },
+            talk: 
+            {   url: `${baseUrl}/animation-library-glb/feminine/expression/F_Talking_Variations_001.glb`,
+                source: "readyPlayerMe"
+            },
+            walk:
+            {   url: `${baseUrl}/animation-library-glb/feminine/locomotion/F_Walk_003.glb`,
+                source: "readyPlayerMe"
+            },
+            run:
+            {   url: `${baseUrl}/animation-library-glb/feminine/locomotion/F_Run_001.glb`,
+                source: "readyPlayerMe"
+            }
+        }
+
     }
 
 }
